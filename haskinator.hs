35f9ba9921897fd0cb52118a822e4f3c0a66515e
JMLTUnderCode <juniormiguel08062@gmail.com>
2023-10-23 21:05:35 -0400

    Modificacion menor de comentarios, insercion de funcion cargar, persistir y crear Oraculo.

--- haskinator.hs
+++ haskinator.hs
@@ -1,11 +1,20 @@
--- Importacion de modulo Oraculo toda las funcionalidades.
--- import Oraculo
-
+-- Importacion de modulo Oraculo y modulos generales para el funcionaimento del programa.
+import Oraculo
 import System.IO
 import qualified Data.Map as Map
+import System.IO.Unsafe ( unsafePerformIO )
+import Data.List (isInfixOf)
 
-data Oraculo = Prediccion String | Pregunta String Opciones
-type Opciones = Map.Map String Oraculo
+-- Funcion encargada de dado un string leido por archivo de texto generar la estructura Oraculo
+cargarOraculo:: String -> Oraculo
+cargarOraculo fileName = do
+    let content = unsafePerformIO (readFile fileName)
+    read content :: Oraculo
+
+-- Funcion que dado un archivo y la estructura de datos Oraculo plasma en el dicho archivo el arbol 
+-- de informacion descrito por Oraculo.
+persistirOraculo:: Oraculo -> String -> IO () 
+persistirOraculo oraculo nameFile = writeFile nameFile (show oraculo) 
 
 main :: IO ()
 main = do
@@ -43,50 +52,65 @@ cliente oraculo = do
 
     opcion <- getLine
     case opcion of
-        "1" -> do 
-            -- Crear oráculo
-            putStrLn ("Ingresa una predicción")
-            p <- getLine
-
-            cliente (Prediccion p)
-        "2" -> do
-            -- Predicción
+        "1" -> do -- Crear oráculo
+            
+            putStrLn ("Ingresa una predicción: ")
+            inputUser <- getLine
+            
+            if not("\t" `isInfixOf` inputUser) then do
+                let newPrediccion = crearOraculo inputUser 
+                cliente newPrediccion
+                else do
+                    putStrLn "Formato de prediccion incorrecta."
+                    cliente oraculo
+                
+        "2" -> do -- Predicción
+            
             cliente oraculo
-        "3" -> do
-            -- Persistir
+            
+        "3" -> do -- Persistir
+            
             putStrLn ("Indica el nombre del archivo")
-            fileName <- getLine
-
+            inputUser <- getLine
+            let fileName = if ".txt" `isInfixOf` inputUser then inputUser else inputUser++".txt"
+            persistirOraculo oraculo fileName
+            
             putStrLn ("Información almacenada en el archivo " ++ fileName)
             cliente oraculo
-        "4" -> do
-            -- Cargar
+            
+        "4" -> do -- Cargar
+            
             putStrLn ("Indica el nombre del archivo")
-            fileName <- getLine
+            inputUser <- getLine
+            let fileName = if ".txt" `isInfixOf` inputUser then inputUser else inputUser++".txt"
+            let newOraculo = cargarOraculo fileName
             
             putStrLn ("Información cargada desde el archivo " ++ fileName)
-            cliente oraculo
-        "5" -> do
-            -- Pregunta crucial
+            cliente newOraculo
+            
+        "5" -> do -- Pregunta crucial
+            
             putStrLn ("Indica la primera cadena")
             pred1 <- getLine
             putStrLn ("Indica la segunda cadena")
             pred2 <- getLine
 
             cliente oraculo
-        "6" -> do
-            -- Estadísticas
+            
+        "6" -> do -- Estadísticas
+            
             putStrLn ("Mínimo: ~")
             putStrLn ("Máximo: ~")
             putStrLn ("Promedio: ~")
 
             cliente oraculo
-        "7" -> do 
-            -- Salir
+            
+        "7" -> do -- Salir
             return ()
-        _   -> do 
-            -- Error
+            
+        _   -> do -- Error
+            
             putStrLn "Opción inválida."
             cliente oraculo
-    
+
     hFlush stdout
