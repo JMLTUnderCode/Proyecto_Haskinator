512abed6104ecda766977932438c99b07df690a0
JMLTUnderCode <juniormiguel08062@gmail.com>
2023-10-26 01:25:20 -0400

    Inclusion de un nuevo caso de prueba para la instancia READ del modulo Oraculo

--- testRead.hs
+++ testRead.hs
@@ -3,15 +3,18 @@ import Oraculo
 main :: IO()
 main = do
   -- Para probar la funcion readData que usa la instancia Read
-  putStrLn "\ntest 1"
+  putStrLn "\n test 1"
   putStrLn $ show $ readData ["a\n\t- b: c\n\t\t- d: e\n\t- f: g\n\t\t- h: i"] 1
   
-  putStrLn "\ntest 2"
+  putStrLn "\n test 2"
   putStrLn $ show $ readData ["Estas bien?\n\t- No: Estas enferma?\n\t\t- No: Comiste bien\n\t\t- Si: Comiste mal"] 1
   
-  putStrLn "\ntest 3"
+  putStrLn "\n test 3"
   putStrLn $ show $ readData ["Prediccion directa"] 1
   
+  putStrLn "\n test 4"
+  putStrLn $ show $ readData ["Es un lenguaje de programacion?\r\n\t- No: HTML\n\t- Si: A que paradigma pertenece?\r\n\t\t- Funcional: Haskell\r\n\t\t- Imperativo: A quien pertenece?\r\n\t\t\t- Google: Go\r\n\t\t\t- Microsoft: C#\r\n\t\t\t- Oracle: Java\r\n\t\t- Logico: Prolog\r"] 1
+
   -- Para probar la instancia read directamente, realizar en consola
   -- cargando el modulo al interprete.
   -- ghci Oraculo.hs
@@ -19,3 +22,4 @@ main = do
   -- >> read "a\n\t- b: c\n\t\t- d: e\n\t- f: g\n\t\t- h: i" :: Oraculo
   -- >> read "hola?\n\t- No: Ayer?\n\t\t- No: tomate\n\t\t- Si: Paracetamol" :: Oraculo
   -- >> read "hola" :: Oraculo
+  -- >> read "Es un lenguaje de programacion?\r\n\t- No: HTML\n\t- Si: A que paradigma pertenece?\r\n\t\t- Funcional: Haskell\r\n\t\t- Imperativo: A quien pertenece?\r\n\t\t\t- Google: Go\r\n\t\t\t- Microsoft: C#\r\n\t\t\t- Oracle: Java\r\n\t\t- Logico: Prolog\r" :: Oraculo
