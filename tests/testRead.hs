61fa47aa6d60901ead2b6c40d9f3df41e739efad
JMLTUnderCode <juniormiguel08062@gmail.com>
2023-10-26 03:00:57 -0400

    Inclusion de nuevo caso de prueba.

--- tests/testRead.hs
+++ tests/testRead.hs
@@ -12,5 +12,18 @@ main = do
   print oraculo2
 
   putStrLn "\ntest 3"
-  let oraculo = read "Prediccion directa" :: Oraculo
-  print oraculo
+  let oraculo3 = read "Prediccion directa" :: Oraculo
+  print oraculo3
+  
+  putStrLn "\ntest 4"
+  let oraculo4 = read "Es un lenguaje de programacion?\r\n\t- No: HTML\n\t- Si: A que paradigma pertenece?\r\n\t\t- Funcional: Haskell\r\n\t\t- Imperativo: A quien pertenece?\r\n\t\t\t- Google: Go\r\n\t\t\t- Microsoft: C#\r\n\t\t\t- Oracle: Java\r\n\t\t- Logico: Prolog\r" :: Oraculo
+  print oraculo4 
+
+  -- Para probar la instancia read directamente, realizar en consola cargando el modulo 
+  -- al interprete.
+  -- ghci Oraculo.hs
+  -- >> import Oraculo
+  -- >> read "a\n\t- b: c\n\t\t- d: e\n\t- f: g\n\t\t- h: i" :: Oraculo
+  -- >> read "hola?\n\t- No: Ayer?\n\t\t- No: tomate\n\t\t- Si: Paracetamol" :: Oraculo
+  -- >> read "hola" :: Oraculo
+  -- >> read "Es un lenguaje de programacion?\r\n\t- No: HTML\n\t- Si: A que paradigma pertenece?\r\n\t\t- Funcional: Haskell\r\n\t\t- Imperativo: A quien pertenece?\r\n\t\t\t- Google: Go\r\n\t\t\t- Microsoft: C#\r\n\t\t\t- Oracle: Java\r\n\t\t- Logico: Prolog\r" :: Oraculo
