590c34eaf66b3d62543f45544b4059f595195f37
JMLTUnderCode <juniormiguel08062@gmail.com>
2023-10-28 03:08:19 -0400

    Implementacion funcional casi completa de PREDECIR(falta rebotar prediccion repetidas), inclusion de la funcion PreguntaCrucial, modificacion general de prompts de haskinator  (interaccion con el chaval)

--- haskinator.hs
+++ haskinator.hs
@@ -2,88 +2,141 @@
 import Oraculo
 import System.IO
 import qualified Data.Map as Map
-import System.IO.Unsafe ( unsafePerformIO )
+import System.IO.Unsafe (unsafePerformIO)
 import Data.List (isInfixOf)
 import Data.Maybe (fromJust)
 import Data.Either
 import Data.Char
 import System.Directory
 
--- Funcion que se encarga de realizar el proceso de prediccion.
--- actualizar :: ?? -> Oraculo
+-- Funcion que determinar si un Oraculo es de tipo Pregunta o es Prediccion.
+esPregunta :: Oraculo -> Bool
+esPregunta (Pregunta _ _) = True
+esPregunta (Prediccion _) = False
 
--- Mostrar el oraculo en version Prediccion.
-predecir :: Oraculo -> IO ()
-predecir (Prediccion prediction) = do
-    putStr("Haskinator : Prediccion: " ++ prediction ++ "\n              Si / No" ++ "\nUsuario    : ")
-    inputUser <- getLine
-    if map toUpper inputUser == "SI" then 
-        putStrLn "¡Soy demasiado genial! Haskinator lo sabe todo."
-        else do 
-            if map toUpper inputUser == "NO" then
-                putStrLn "Haskinator : He fallado! Cual era la respuesta correcta?"
-                -- DEBEMOS ACTUALIZAR
+-- Funcion encargada de actualizar el oraculo principal para el caso de que el usuario
+-- diga que una prediccion dada es incorrecta.
+actualizarOraculo :: (String, Oraculo) -> Oraculo -> Oraculo -> Oraculo
+actualizarOraculo tuple toBeUpdate oracle = do
+    let mapToList = Map.toList $ opciones oracle
+    if tuple `elem` mapToList then do
+        let updatingOptions =  Map.insert (fst tuple) toBeUpdate $ opciones oracle
+        let mainQuestion = pregunta oracle
+        Pregunta mainQuestion updatingOptions
+        else do
+            let valuesFromOptions = Map.elems $ opciones oracle
+            let updatingOraculo = map (\x -> if esPregunta x then actualizarOraculo tuple toBeUpdate x else x) valuesFromOptions
+            let updatedOptions = Map.keys $ opciones oracle
+            let newOracleOptions = zip updatedOptions updatingOraculo
+            let firstQuestion = pregunta oracle
+            Pregunta firstQuestion $ Map.fromList newOracleOptions
+
+-- Funcion encargada de actualizar el oraculo principal para el caso de que el usuario
+-- indique "Ninguna" de las opciones propuestas a una pregunta.
+actualizarOraculo' :: (String, Oraculo) -> String -> Oraculo -> Oraculo
+actualizarOraculo' toBeUpdate question oracle = do
+    let mapToList = Map.toList $ opciones oracle
+    if question == pregunta oracle then do
+        let newOracle = uncurry Map.insert toBeUpdate $ opciones oracle
+        Pregunta question newOracle
+        else do
+            let elementTarget = filter (\(y, x) -> esPregunta x && question == pregunta x) mapToList
+            if not (null elementTarget) then do
+                let optionUpdate = opciones $ snd (head elementTarget)
+                let newInnerOracle = uncurry Map.insert toBeUpdate optionUpdate
+                let newOracle = Map.insert (fst (head elementTarget)) (Pregunta question newInnerOracle) $ opciones oracle
+                let mainQuestion = pregunta oracle
+                Pregunta mainQuestion newOracle
                 else do
-                    putStrLn $ "Haskinator : La opcion '" ++ inputUser ++ "' no es valida."
-                    predecir (Prediccion prediction)
+                    let elementsOracle = Map.elems $ opciones oracle
+                    let updatingOracle = map (\x -> if esPregunta x then actualizarOraculo' toBeUpdate question x else x) elementsOracle
+                    let updatedOptions = Map.keys $ opciones oracle
+                    let newOracleOptions = zip updatedOptions updatingOracle
+                    let firstQuestion = pregunta oracle
+                    Pregunta firstQuestion $ Map.fromList newOracleOptions
+
+-- Funcion que se encarga de realizar el proceso de prediccion. Muestra las interacciones con
+-- el usuario, solicita datos y realiza llamados a funciones de actualizacion de oraculos.
+predecir :: Oraculo -> Oraculo -> String -> IO Oraculo
+predecir (Prediccion prediction) originalOracle lastOption = do
+    hFlush stdout
+    putStr $ "   Haskinator : Prediccion: " ++ prediction ++ "\n                 Si / No" ++ "\n   Usuario    : "
+    inputUser <- getLine
+    if map toUpper inputUser == "SI" then do
+        putStrLn "   ¡Habeis flipao' colores tio! Soy la ostia a que si?"
+        return originalOracle
+        else do 
+            if map toUpper inputUser == "NO" then do
+                putStr "   Haskinator : ¡Me cago en la ostia tio! Cual es la puñetera respuesta?\n   Usuario    : "
+                expectedAnswer <- getLine
+
+                putStr $ "   Haskinator : Que pregunta distingue a '"++ expectedAnswer ++"' de las otras opciones?\n   Usuario    : "
+                distinguishedQuestion <- getLine
+
+                let questionPrompt = "   Haskinator : Cual es la respuesta a '"++ distinguishedQuestion ++"' para '"
+                putStr $ questionPrompt ++ expectedAnswer ++ "'?\n   Usuario    : "
+                optionForExpectedAnswer <- getLine
+                
+                -- MAL PRINTEO (SOLVENTAR)
+                putStr $ questionPrompt ++ prediction
+                putStr "'?\n   Usuario    : "
+                optionForGivenPrediction <- getLine
+
+                let newQuestion = Pregunta distinguishedQuestion (Map.fromList [(optionForExpectedAnswer, Prediccion expectedAnswer), (optionForGivenPrediction, Prediccion prediction)])
+                let updatedOraculo = actualizarOraculo (lastOption, Prediccion prediction) newQuestion originalOracle
+                putStrLn "   Graciah chaval! Pero sobre todo agradecido con el de arriba Papa Dio'"
+                return updatedOraculo
+                else do
+                    putStrLn $ "   Haskinator : Que dices chaval! La opcion '" ++ inputUser ++ "' no es valida."
+                    predecir (Prediccion prediction) originalOracle lastOption
 
 -- Mostrar un Oraculo en version Pregunta, se muestra la misma y las opciones que conllevan esa pregunta.
-predecir oracle = do
+predecir oracle originalOracle lastOption = do
     let options = opciones oracle
     let answer = pregunta oracle
     let availableOptions = Map.keys options
-    putStr $ "Haskinator : " ++ answer ++ "\n              " ++ concatMap ( ++ " / ") availableOptions ++ "Ninguna\nUsuario    : "
+    putStr $ "   Haskinator : " ++ answer ++ "\n                 " ++ concatMap ( ++ " / ") availableOptions ++ "Ninguna\n   Usuario    : "
     inputUser <- getLine
     -- Verificamos que la opcion sea valida.
     if inputUser `isInfixOf` concat availableOptions then do
-        predecir $ respuesta oracle inputUser
+        let newOracle = respuesta oracle inputUser  
+        predecir newOracle originalOracle inputUser        
         else do
             -- En caso de marcar NINGUNA luego de mostrar las opciones disponibles.
             if map toUpper inputUser == "NINGUNA" then do
-                putStrLn "Haskinator : He fallado! Cual era la respuesta correcta?"
-                -- DEBEMOS ACTUALIZAR
+                putStr "   Haskinator : ¡Me cago en la ostia tio! Cual es la puñetera respuesta?\n   Usuario    : "
+                expectedAnswer <- getLine
+                putStr $ "   Haskinator : " ++ answer ++ "\n   Usuario    : "
+                optionForExpectedAnswer <- getLine
+                let newPrediction = Prediccion expectedAnswer
+                let updatedOraculo = actualizarOraculo' (optionForExpectedAnswer, newPrediction) answer originalOracle
+                putStrLn "   Graciah chaval! Pero sobre todo agradecido con el de arriba Papa Dio'"
+                return updatedOraculo
                 else do 
-                    putStrLn $ "La opcion '" ++ inputUser ++ "' no existe."
-                    predecir oracle
-{-
-EJEMPLOS
-  En caso de responder "No" a una prediccion:
-    1. Pedir la RESPUESTA correcta. (Prediccion final)
-    2. Pedir la PREGUNTA que distinguie a la RESPUESTA de (1)
-    3. Pedir la RESPUESTA a la PREGUNTA de (2) asociada a la respuesta de (1)
-    4. Pedir la RESPUESTA a la PREGUNTA de (2) asociada a la respuesta de la prediccion "errada"
-    Haskinator : Prediccion: HTML
-                 Si / No
-    Usuario    : No
-    Haskinator : He fallado! Cual era la respuesta correcta?
-    Usuario    : CSS
-    Haskinator : Que pregunta distingue a CSS de las otras opciones?
-    Usuario    : Que tipo de lenguaje es?
-    Haskinator : Cual es la respuesta a "Que tipo de lenguaje es?" para CSS?
-    Usuario    : De definicion de estilo
-    Haskinator : Cual es la respuesta a "Que tipo de lenguaje es?" para HTML?
-    Usuario    : De marcado
--}
-{-
-  En caso de responder NINGUNA en el proceso de seleccion de opciones:
-    1. Pedir la RESPUESTA correcta. (Prediccion final)
-    2. Pedir la RESPUESTA a la PREGUNTA que no posee la respuesta (1)
-  
-    Haskinator : Es un lenguaje de programacion?
-                 Si / No
-    Usuario    : Si
-    Haskinator : A que paradigma pertenece?
-                 Imperativo / Funcional / Logico
-    Usuario    : Imperativo
-    Haskinator : A quien pertenece el lenguaje?
-                 Oracle / Microsoft
-    Usuario    : ninguna
-    Haskinator : He fallado! Cual era la respuesta correcta?
-    Usuario    : Go
-    Haskinator : A quien pertenece el lenguaje?
-    Usuario    : Google
--}
-  
+                    putStrLn $ "   La opcion '" ++ inputUser ++ "' no existe."
+                    predecir oracle originalOracle lastOption
+
+-- Funcion que dado un oraculo y dos strings retorna el ancestro comun mas 
+-- bajo en el arbol informacion descrito por Oraculo.
+preguntaCrucial :: Oraculo -> String -> String -> Maybe (String, String, String)
+preguntaCrucial oraculo pred1 pred2 = do
+    c1 <- obtenerCadena oraculo pred1
+    c2 <- obtenerCadena oraculo pred2
+    if null c1 || null c2 then Nothing
+    else do
+        let cC = caminoComun c1 c2
+        let l = length cC
+        let c1Diff = drop l c1
+        let c2Diff = drop l c2
+        Just (fst $ head c1Diff, snd $ head c1Diff, snd $ head c2Diff)
+
+caminoComun :: [(String, String)] -> [(String, String)] -> [(String, String)]
+caminoComun [] _ = []
+caminoComun _ [] = []
+caminoComun (xh : xt) (yh : yt) 
+    | xh == yh =  xh : caminoComun xt yt
+    | otherwise = []
+
 -- Funcion que dado un archivo y la estructura de datos Oraculo plasma en el dicho archivo el arbol 
 -- de informacion descrito por Oraculo.
 persistirOraculo:: Oraculo -> String -> IO () 
@@ -98,7 +151,7 @@ cargarOraculo fileName = do
 main :: IO ()
 main = do
     header
-    cliente (Prediccion "")
+    cliente (Prediccion "Yo solo se que no se nada chaval. d:v")
 
 header :: IO ()
 header = do
@@ -112,52 +165,49 @@ header = do
     putStrLn "-------------------------------------------------------------------------------"
     putStrLn "                                    AUTORES"
     putStrLn "-------------------------------------------------------------------------------"
-    putStrLn "16-10371 | Daniel Figueroa"
-    putStrLn "17-10303 | Junior Lara"
-    putStrLn "18-10938 | Astrid Alvarado"
-    putStrLn "15-11377 | Carlos Sivira"
-    putStrLn "-------------------------------------------------------------------------------"
+    putStrLn "   16-10371 | Daniel Figueroa"
+    putStrLn "   17-10303 | Junior Lara"
+    putStrLn "   18-10938 | Astrid Alvarado"
+    putStrLn "   15-11377 | Carlos Sivira"
 
 cliente :: Oraculo -> IO ()
 cliente oraculo = do
-    putStrLn "\nSelecciona una opción:\n"
-    putStrLn "1. Crear un oráculo nuevo"
-    putStrLn "2. Predecir"
-    putStrLn "3. Persistir"
-    putStrLn "4. Cargar"
-    putStrLn "5. Consultar pregunta crucial"
-    putStrLn "6. Estadísticas"
-    putStrLn "7. Salir\n"
+    putStrLn "-------------------------------------------------------------------------------"
+    putStrLn "                        ***  Selecciona una opción: ***"
+    putStrLn "           1. Crear un oráculo nuevo     5. Consultar pregunta crucial"
+    putStrLn "           2. Predecir                   6. Estadísticas"
+    putStrLn "           3. Persistir                  7. Salir"
+    putStrLn "           4. Cargar"
+    putStr "   >> "
 
     opcion <- getLine
     case opcion of
         "1" -> do -- Crear oráculo
-            putStrLn "Ingresa una predicción: "
+            putStr "   Ingresa una predicción: "
             inputUser <- getLine
 
             if not ("\t" `isInfixOf` inputUser) then do
                 let newPrediccion = crearOraculo inputUser
                 cliente newPrediccion
                 else do
-                    putStrLn "Formato de prediccion incorrecta."
+                    putStrLn "   Formato de prediccion incorrecta."
                     cliente oraculo
 
         "2" -> do -- Predecir
-            predecir oraculo
-            
-            cliente oraculo
+            newOraculo <- predecir oraculo oraculo ""
+            cliente newOraculo
 
         "3" -> do -- Persistir
-            putStrLn "Indica el nombre del archivo"
+            putStr "   Indica el nombre del archivo: "
             inputUser <- getLine
             let fileName = if ".txt" `isInfixOf` inputUser then inputUser else inputUser++".txt"
             persistirOraculo oraculo fileName
 
-            putStrLn $ "Información almacenada en el archivo " ++ fileName
+            putStrLn $ "   Información almacenada en el archivo " ++ fileName
             cliente oraculo
 
         "4" -> do -- Cargar
-            putStrLn "Indica el nombre del archivo"
+            putStr "   Indica el nombre del archivo: "
             inputUser <- getLine
 
             -- Agregamos la extension .txt en caso de que el archivo no lo tenga.
@@ -165,39 +215,40 @@ cliente oraculo = do
 
             -- Verificando que el archivo exista.
             if not (unsafePerformIO (doesFileExist $ "./" ++ fileName)) then do
-                putStrLn $ "El nombre '" ++ fileName ++ "' no existe o no esta asociado a un archivo .txt."
+                putStrLn $ "   El nombre '" ++ fileName ++ "' no existe o no esta asociado a un archivo .txt."
                 cliente oraculo
             else do
                 let newOraculo = cargarOraculo fileName
-                putStrLn $ "Información cargada desde el archivo " ++ fileName
+                putStrLn $ "   Información cargada desde el archivo " ++ fileName
                 cliente newOraculo
             
         "5" -> do -- Pregunta crucial
-            putStrLn "Indica la primera cadena"
+            putStr "   Indica la primera cadena: "
             pred1 <- getLine
-            putStrLn "Indica la segunda cadena"
+            putStr "   Indica la segunda cadena: "
             pred2 <- getLine
 
             case preguntaCrucial oraculo pred1 pred2 of
                 Just (pregunta, opcion1, opcion2) -> do
-                    putStrLn $ "Pregunta: '" ++ pregunta ++ "'"
-                    putStrLn $ "La opción '" ++ opcion1 ++ "' lleva a '" ++ pred1 ++ "'"
-                    putStrLn $ "La opción '" ++ opcion2 ++ "' lleva a '" ++ pred2 ++ "'"
-                Nothing -> putStrLn "No se encontró una pregunta crucial."
+                    putStrLn $ "   Pregunta: '" ++ pregunta ++ "'"
+                    putStrLn $ "   La opción '" ++ opcion1 ++ "' lleva a '" ++ pred1 ++ "'"
+                    putStrLn $ "   La opción '" ++ opcion2 ++ "' lleva a '" ++ pred2 ++ "'"
+                Nothing -> putStrLn "   No se encontró una pregunta crucial."
 
             cliente oraculo
 
         "6" -> do -- Estadísticas
             let (oMin, oMax, oAvg) = obtenerEstadisticas oraculo
-            putStrLn $ "min: " ++ show oMin ++ "       max: " ++ show oMax ++ "      avg: " ++ show oAvg
+            putStrLn $ "   min: " ++ show oMin ++ "       max: " ++ show oMax ++ "      avg: " ++ show oAvg
 
             cliente oraculo
 
         "7" -> do -- Salir
+            putStrLn "   Haskinator Españolete. Version 1.0. "
             return ()
 
         _   -> do -- Error
-            putStrLn "Opción inválida."
+            putStrLn "   Opción inválida."
             cliente oraculo
 
     hFlush stdout
